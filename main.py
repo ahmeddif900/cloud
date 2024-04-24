c026993a01287f2f304dbd9ae3f463573a6d2086
ahmeddif900 <167941298+ahmeddif900@users.noreply.github.com>
2024-04-24 20:25:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from collections import Counter
 
-# Download NLTK stopwords if not already downloaded
 nltk.download('stopwords')
 nltk.download('punkt')
 
